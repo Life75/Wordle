d5c8faa7a262fb53cbbf2c0e6bf7c71b1baf3a02
awashington <awashington@healthqlix.com>
2022-04-04 11:35:22 -0400

    fleshed out letter

--- Wordle/src/components/Letter.vue
+++ Wordle/src/components/Letter.vue
@@ -5,11 +5,11 @@
             <div class="min-h-screen flex justify-center items-center">
                 <!-- Start of component -->
                 <div
-                    class="max-w-sm bg-white border-2 border-gray-300 p-3 rounded-md tracking-wide shadow-lg"
+                    class="max-w-sm  bg-white border-2 border-gray-300 p-2 rounded-md tracking-wide shadow-lg" 
                 >
-                    <div id="header" class="flex items-center mb-4">
-                        <div class="text-xl">
-                            A
+                    <div id="header" class="flex items-center  ">
+                        <div class="text-xl border-orange-400 focus:border-cyan-300" @focusout="lockOut()">
+                            <input class=" ml-1 mb-2 border-none outline-none border-2 w-4 caret-white" cursor maxlength="1" :disabled="lockedOut" v-model="letter">
                         </div>
                     </div>
                 </div>
@@ -22,6 +22,19 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from '@vue/reactivity';
+
+
+var letter = ref('')
+var lockedOut = false;
+
+function lockOut() {
+    if(letter.value != '') {
+        lockedOut = true;
+    }
+}
+
+
 
 </script>
 
